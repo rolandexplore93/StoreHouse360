251117e5aa59f762051d42ff1ffe59005f315acf
Roland2rule <roland2rule@gmail.com>
2024-12-30 18:11:01 +0000

    Reapply "feat (global): Add a global query filter on soft delete entity"
    This reverts commit e1b3a87b2fb8c2a80e05b18c8dd8f31a1119718b.

---
+++ StoreHouse360.Infrastructure/Persistence/Database/ApplicationDbContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace StoreHouse360.Infrastructure.Persistence.Database
+{
+    public static class ApplicationDbContextExtensions
+    {
+        public static void ApplyGlobalFilters<TkInterface>(this ModelBuilder modelBuilder, Expression<Func<TkInterface, bool>> expresson)
+        {
+            var entityTypes = modelBuilder.Model.GetEntityTypes();
+            var entities = entityTypes
+                .Where(e => e.ClrType.GetInterface(typeof(TkInterface).Name) != null)
+                .Select(e => e.ClrType);
+
+            foreach (var entity in entities)
+            {
+                var newParameter = Expression.Parameter(entity);
+                var newBody = ReplacingExpressionVisitor.Replace(expresson.Parameters.Single(), newParameter, expresson.Body);
+                modelBuilder.Entity(entity).HasQueryFilter(Expression.Lambda(newBody, newParameter));
+            }
+        }
+    }
+}
