228c722189d543c5b5e2f2935368f7041c23983c
Roland2rule <roland2rule@gmail.com>
2025-01-03 15:33:07 +0000

    Apply IncludeFilter() from Z.EntityFramework.Plus library when retrieving Payments to include related entities conditionally.

--- StoreHouse360.Infrastructure/Repositories/PaymentRepository.cs
+++ StoreHouse360.Infrastructure/Repositories/PaymentRepository.cs
@@ -4,6 +4,7 @@ using StoreHouse360.Domain.Entities;
 using StoreHouse360.Infrastructure.Persistence.Database.Models;
 using StoreHouse360.Infrastructure.Persistence.Database;
 using Microsoft.EntityFrameworkCore;
+using Z.EntityFramework.Plus;
 
 namespace StoreHouse360.Infrastructure.Repositories
 {
@@ -15,7 +16,9 @@ namespace StoreHouse360.Infrastructure.Repositories
 
         protected override IQueryable<PaymentDb> GetIncludedDatabaseSet()
         {
-            return dbSet.Include(p => p.Currency).Include(p => p.CurrencyAmounts);
+            return dbSet
+                .Include(payment => payment.Currency)
+                .IncludeFilter(payment => payment.CurrencyAmounts.Where(p => p.Key == CurrencyAmountKey.Payment));
         }
     }
 }
