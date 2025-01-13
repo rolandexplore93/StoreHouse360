b0ba0eec812d3886b11f67887c89f4fe7706e8aa
Roland2rule <roland2rule@gmail.com>
2025-01-13 22:10:19 +0000

    Refactors inventory query handler to execute only the repository query

--- StoreHouse360.Application/Queries/Warehouses/InventoryWarehouseQuery.cs
+++ StoreHouse360.Application/Queries/Warehouses/InventoryWarehouseQuery.cs
@@ -4,7 +4,6 @@ using StoreHouse360.Application.Common.Models;
 using StoreHouse360.Application.Queries.Common;
 using StoreHouse360.Application.Repositories;
 using StoreHouse360.Domain.Aggregations;
-using StoreHouse360.Domain.Entities;
 
 namespace StoreHouse360.Application.Queries.Warehouses
 {
@@ -15,34 +14,16 @@ namespace StoreHouse360.Application.Queries.Warehouses
     public class InventoryWarehouseQueryHandler : IRequestHandler<InventoryWarehouseQuery, IPaginatedCollections<AggregateProductQuantity>>
     {
         private readonly IProductMovementRepository _productMovementRepository;
-        private readonly IProductRepository _productRepository;
 
-        public InventoryWarehouseQueryHandler(IProductMovementRepository productMovementRepository, IProductRepository productRepository)
+        public InventoryWarehouseQueryHandler(IProductMovementRepository productMovementRepository)
         {
             _productMovementRepository = productMovementRepository;
-            _productRepository = productRepository;
         }
         public async Task<IPaginatedCollections<AggregateProductQuantity>> Handle(InventoryWarehouseQuery request, CancellationToken cancellationToken)
         {
-            var aggregates = _productMovementRepository.AggregateProductsQuantities(request.Filters).ToList();
+            var aggregates = _productMovementRepository.AggregateProductsQuantities(request.Filters).AsPaginatedQuery(request.Page, request.PageSize);
 
-            var productIds = aggregates.Select(aggregate => aggregate.Product!.Id);
-
-            var products = await _productRepository.GetAllAsync(new GetAllOptions<Product> { IncludeRelations = true });
-
-            var filteredProducts = products
-                .Where(product => productIds.Any(id => product.Id == id))
-                .ToList();
-
-            var aggregatesWithFullProduct = aggregates
-                .Zip(filteredProducts)
-                .Select(entry => entry.First.AddProduct(entry.Second));
-
-            var paginatedQuery = aggregatesWithFullProduct
-                .AsQueryable()
-                .AsPaginatedQuery(request.Page, request.PageSize);
-
-            return paginatedQuery;
+            return aggregates;
         }
     }
 }
