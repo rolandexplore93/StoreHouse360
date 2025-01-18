ab932dc25057d189c70bf72c966c399c1343c8df
Roland2rule <roland2rule@gmail.com>
2025-01-18 07:34:26 +0000

    Adds default currency seeder

---
+++ StoreHouse360.Infrastructure/Persistence/Database/SeedData/Currencies/MainCurrencySeeder.cs
@@ -0,0 +1,35 @@
+﻿using StoreHouse360.Application.Services.Settings;
+using StoreHouse360.Infrastructure.Persistence.Database.Models;
+
+namespace StoreHouse360.Infrastructure.Persistence.Database.SeedData.Currencies
+{
+    public class MainCurrencySeeder : ISeedData
+    {
+        public void Seed(ApplicationDbContext dbContext, IAppSettingsProvider settingsProvider)
+        {
+            var settings = settingsProvider.Get();
+            var mainCurrency = dbContext.Currencies.FirstOrDefault(account => account.Id == settings.DefaultCurrencyId);
+
+            if (mainCurrency != null)
+            {
+                return;
+            }
+
+            using (var transaction = dbContext.Database.BeginTransaction())
+            {
+                var entry = dbContext.Currencies.Add(new CurrencyDb()
+                {
+                    Name = "Main Sales",
+                    Symbol = "SYP",
+                    Factor = 1
+                });
+                dbContext.SaveChanges();
+                settings.DefaultCurrencyId = entry.Entity.Id;
+
+                settingsProvider.Configure(settings);
+
+                transaction.Commit();
+            }
+        }
+    }
+}
