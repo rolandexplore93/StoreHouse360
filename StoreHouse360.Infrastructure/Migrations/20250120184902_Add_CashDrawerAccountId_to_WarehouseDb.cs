6a4d8b0f838bbb02ee40a64e996d5179e781592b
Roland2rule <roland2rule@gmail.com>
2025-01-20 19:02:28 +0000

    (feat) Creates cash drawer with each warehouse created

---
+++ StoreHouse360.Infrastructure/Migrations/20250120184902_Add_CashDrawerAccountId_to_WarehouseDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreHouse360.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_CashDrawerAccountId_to_WarehouseDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CashDrawerAccountId",
+                table: "Warehouses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CashDrawerAccountId",
+                table: "Warehouses");
+        }
+    }
+}
