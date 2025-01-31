7a52a64f58c1f674a91469ff5d322a6091ffcb3f
Roland2rule <roland2rule@gmail.com>
2025-01-31 05:09:31 +0000

    Adds main cash drawer as default account statement
    - Adds product full name

--- StoreHouse360.Presentation/Controllers/Api/AccountingController.cs
+++ StoreHouse360.Presentation/Controllers/Api/AccountingController.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StoreHouse360.Application.Queries.Accounting;
+using StoreHouse360.Application.Settings;
 using StoreHouse360.DTO.Accounting;
 using StoreHouse360.DTO.Common;
 using StoreHouse360.Presentation.DTO.Common.Responses;
@@ -22,6 +23,14 @@ namespace StoreHouse360.Controllers.Api
             var accountStatement = await Mediator.Send(new GetAccountStatementQuery(accountId));
             var viewmodel = accountStatement.ToViewModel<AccountStatementVM>(_mapper);
             return Ok(viewmodel);
+        }
+
+        [HttpGet("accountStatement/cashDrawer")]
+        public async Task<ActionResult<BaseResponse<AccountStatementVM>>> AccountStatement([FromServices] AppSettings settings)
+        {
+            var accountStatement = await Mediator.Send(new GetAccountStatementQuery(settings.DefaultMainCashDrawerAccountId));
+            var vm = accountStatement.ToViewModel<AccountStatementVM>(_mapper); 
+            return Ok(vm);
         }
     }
 }
