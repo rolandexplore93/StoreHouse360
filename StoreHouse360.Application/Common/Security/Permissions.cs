671dbf3bff6e4b936d3a868226ac9e7757b1255c
Roland2rule <roland2rule@gmail.com>
2025-02-15 22:52:38 +0000

    Add policy helper

---
+++ StoreHouse360.Application/Common/Security/Permissions.cs
@@ -0,0 +1,63 @@
+﻿namespace StoreHouse360.Application.Common.Security
+{
+    public class Permissions
+    {
+        private const string AllPermissionKey = "all";
+        private const string NonePermissionKey = "none";
+        public bool AllPermissions { get; set; }
+        public bool None { get; set; }
+
+        public readonly IList<Policy> Policies = new List<Policy>();
+
+        // Add unique policy to Policies collection property
+        public void AddPolicy(Policy policy)
+        {
+            if (Policies.Contains(policy)) return;
+
+            Policies.Add(policy);
+        }
+
+        // Merge permissions
+        public void Merge(Permissions permissions)
+        {
+            AllPermissions = AllPermissions || permissions.AllPermissions;
+            None = None || permissions.None;
+
+            foreach (var permissionsPolicy in permissions.Policies)
+            {
+                AddPolicy(permissionsPolicy);
+            }
+        }
+
+        public static Permissions From(string permission)
+        {
+            var permissions = new Permissions();
+
+            foreach (var policy in permission.Split(PolicyHelper.PoliciesSeparator))
+            {
+                if (policy.Equals(AllPermissionKey, StringComparison.OrdinalIgnoreCase))
+                    permissions.AllPermissions = true;
+                else if (policy.Equals(NonePermissionKey, StringComparison.OrdinalIgnoreCase))
+                    permissions.None = true;
+                else
+                    permissions.AddPolicy(PolicyHelper.CreatePolicyFromString(policy));
+            }
+
+            return permissions;
+        }
+
+        public override string ToString()
+        {
+            if (AllPermissions)
+            {
+                return AllPermissionKey;
+            }
+            else if (None)
+            {
+                return NonePermissionKey;
+            }
+
+            return string.Join(PolicyHelper.PoliciesSeparator, PolicyHelper.PoliciesToString(Policies));
+        }
+    }
+}
---
+++ StoreHouse360.Application/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,11 @@
+﻿using StoreHouse360.Domain.Exceptions;
+
+namespace StoreHouse360.Application.Exceptions
+{
+    public class ForbiddenAccessException : BaseException
+    {
+        public ForbiddenAccessException() : base("Forbidden Access", StatusCodes.ForbiddenAccessExceptionCode)
+        {
+        }
+    }
+}
