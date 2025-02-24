59dd4e5ced6c278372fb2698aea1ee4d36e4de3a
Roland2rule <roland2rule@gmail.com>
2025-02-24 05:44:34 +0000

    Adds product model to notification view model

--- StoreHouse360.Presentation/Controllers/Api/NotificationsController.cs
+++ StoreHouse360.Presentation/Controllers/Api/NotificationsController.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using StoreHouse360.Application.Common.Models;
 using StoreHouse360.Application.Queries.Notifications;
+using StoreHouse360.Application.Queries.Products;
 using StoreHouse360.DTO.Common;
 using StoreHouse360.DTO.Notifications;
 using StoreHouse360.DTO.Pagination;
+using StoreHouse360.DTO.Products;
 using StoreHouse360.Presentation.DTO.Common.Responses;
 
 namespace StoreHouse360.Controllers.Api
@@ -20,7 +23,33 @@ namespace StoreHouse360.Controllers.Api
         {
             var query = _mapper.Map<GetAllNotificationsQuery>(request);
             var notificationsPage = await Mediator.Send(query);
-            return Ok(notificationsPage.ToViewModel<NotificationVM>(_mapper));
+
+            var productsQuery = new GetAllProductsQuery();
+            var products = (await Mediator.Send(productsQuery))
+                .AsEnumerable()
+                .Where(product => notificationsPage.Any(notify => notify.ObjectId == product.Id))
+                .Select(productEntity => _mapper.Map<ProductVM>(productEntity));
+
+            var notificationsVM = notificationsPage
+                .Select(notification => new NotificationVM
+                {
+                    Id = notification.Id,
+                    NotificationType = notification.NotificationType,
+                    Product = products.First(product => product.Id == notification.ObjectId),
+                    IsValid = notification.IsValid
+                });
+
+
+            var notificationsVMPage = new PaginationVM<NotificationVM>
+            {
+                Data = notificationsVM,
+                CurrentPage = notificationsPage.CurrentPage,
+                PageSize = notificationsPage.PageSize,
+                PagesCount = notificationsPage.PagesCount,
+                RowsCount = notificationsPage.RowsCount,
+            };
+
+            return Ok(notificationsVMPage);
         }
     }
 }
--- StoreHouse360.Presentation/DTO/Notifications/NotificationVM.cs
+++ StoreHouse360.Presentation/DTO/Notifications/NotificationVM.cs
@@ -1,13 +1,15 @@
 ﻿using StoreHouse360.Application.Common.Mappings;
 using StoreHouse360.Domain.Entities;
 using StoreHouse360.DTO.Common;
+using StoreHouse360.DTO.Products;
 
 namespace StoreHouse360.DTO.Notifications
 {
     public class NotificationVM : IMapFrom<Notification>, IViewModel
     {
         public int Id { get; set; }
-        public int ObjectId { get; set; }
+        public ProductVM Product { get; set; }
         public NotificationType NotificationType { get; set; }
+        public bool IsValid { get; set; }
     }
 }
